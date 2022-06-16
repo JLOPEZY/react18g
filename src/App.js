ebd61b99fb864a60c337d41a3b901366dbe557e3
JLOPEZY <jylopezy@gmail.com>
2022-06-16 17:06:06 -0500

    Actividad 3 interruptor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import './App.css';
 
@@ -13,9 +13,10 @@ function App() {
   //[1] = función que actualiza el state
   //si `useState`recibe un argumento,este será el valor incial del estado
   //si no el estado es undefined
-  const [count, setCount] = useState(0)
- 
-   // ARGUMENTO 1: CALLBACK
+  const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+
+  // ARGUMENTO 1: CALLBACK
   // ARGUMENTO 2: nada, [], [count]
   // Caso vacío: el callback del useEffect se ejecuta cada que algo cambie (estado)
   // Caso []: el callback del useEffect se ejecuta solo la primera vez que carga el componente
@@ -29,17 +30,17 @@ function App() {
   //     "ME EJECUTO LA PRIMERA VEZ QUE EL COMPONENTE SE CARGA Y CADA VEZ QUE EL ESTADO SE ACTUALIZA"
   //   );
   // }, [count]);
-// ----------------------------------------------
+  // ----------------------------------------------
   // console.log(count, 'COUNT')
   // console.log(setCount, 'SET_COUNT')
-  useEffect(() => {
-    console.log(
-      "ME EJECUTO LA PRIMERA VEZ Y SIEMPRE QUE HAYA UNA ACTUALIZACIÓN DEL COMPONENTE"
-    );
-  });
+  // useEffect(() => {
+  //   console.log(
+  //     "ME EJECUTO LA PRIMERA VEZ Y SIEMPRE QUE HAYA UNA ACTUALIZACIÓN DEL COMPONENTE"
+  //   );
+  // });
 
 
-    const handleClick = (increase) => {
+  const handleClick = (increase) => {
     let newCount = count;
     if (increase) {
       newCount = newCount + 1;
@@ -53,7 +54,6 @@ function App() {
   const isMultiple = count % 5 === 0 && count !== 0 ? true : false;
   const countType = isMultiple ? "Es multiplo de 5" : "No es multiplo de 5";
   const countClass = isMultiple ? "active" : "inactivo";
-  
 
 
   // const handleClick = () => {
@@ -65,27 +65,37 @@ function App() {
   //   setCount(count - 1)
 
   // } 
-
+  const squareClass = isOn ? "square-on" : "square-off"
+  const squareText = isOn ? "ON" : "OFF"
   return (
     <div className="App">
       <header className="App-header">
 
         <Title text="Aaron"></Title>
 
-        <FullName firstName="Yusef" lastName='Lopez'></FullName>
+        <FullName firstName="Yusef" lastName="Lopez"></FullName>
 
         <p className={countClass}>
           Counter: {count}, {countType}
         </p>
 
         <button onClick={() => handleClick(true)}>Increase</button>
-        
+
         {/* <button onClick={handleClick}>Increase</button>
         <button onClick={handleClick2}>Decrease</button> */}
-
-        <div className="square">off</div>
-        
-
+        {/* TEMPLATE INTERPOLACION */}
+        {/* <div className={`square ${isOn ? "square-on" : "square-off"}`}>{isOn ? "ON" : "OFF"}
+        </div>   */}
+        <div className={`square ${squareClass}`}>{squareText}</div>
+        <div 
+          style={{
+          display: "flex",
+        }}
+        >
+          <button onClick={() => setIsOn(!isOn)}>Interruptor</button>
+          {/* <button onClick={() => setIsOn(true)}>ON</button>
+          <button onClick={() => setIsOn(false)}>OFF</button> */}
+        </div>
       </header>
     </div>
   );
