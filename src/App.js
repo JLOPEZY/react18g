b092e59190dca5ef0d8549a3b6e07ae380de9a57
JLOPEZY <jylopezy@gmail.com>
2022-06-22 16:27:45 -0500

    Formularios

--- src/App.js
+++ src/App.js
@@ -1,190 +1,109 @@
-import { useState } from 'react';
-
-import './App.css';
-
 // Components
-// import Title from './components/Title';
-// import FullName from './components/fullName';
+import Card from "./components/Card";
 
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [itemActive, setItemActive] = useState(null);
+  const [firstName, setFirstName] = useState ("");
+  const [lastName, setLastName] = useState ("");
+  const [age, setAge] = useState ("");
+  const [gender, setGender] = useState ("");
 
-  const isActive = (itemNumber) => itemNumber === itemActive;
-
-  //local state
-  //[0] = valor del state
-  //[1] = función que actualiza el state
-  //si `useState`recibe un argumento,este será el valor incial del estado
-  //si no el estado es undefined
-  // const [count, setCount] = useState(0);
-  // const [isOn, setIsOn] = useState(false);
-
-  // ARGUMENTO 1: CALLBACK
-  // ARGUMENTO 2: nada, [], [count]
-  // Caso vacío: el callback del useEffect se ejecuta cada que algo cambie (estado)
-  // Caso []: el callback del useEffect se ejecuta solo la primera vez que carga el componente
-  // Caso [state]:  el callback del useEffect se ejecuta la primera vez y cada que se actualice el estado que se incluye
-  // useEffect(() => {
-  //   console.log("ME EJECUTO LA PRIMERA VEZ QUE EL COMPONENTE SE CARGA");
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log(
-  //     "ME EJECUTO LA PRIMERA VEZ QUE EL COMPONENTE SE CARGA Y CADA VEZ QUE EL ESTADO SE ACTUALIZA"
-  //   );
-  // }, [count]);
-  // ----------------------------------------------
-  // console.log(count, 'COUNT')
-  // console.log(setCount, 'SET_COUNT')
-  // useEffect(() => {
-  //   console.log(
-  //     "ME EJECUTO LA PRIMERA VEZ Y SIEMPRE QUE HAYA UNA ACTUALIZACIÓN DEL COMPONENTE"
-  //   );
-  // });
-
-
-  // const handleClick = (increase) => {
-  //   let newCount = count;
-  //   if (increase) {
-  //     newCount = newCount + 1;
-  //   } else {
-  //     newCount = newCount - 1;
-  //   }
-  //   setCount(newCount);
-
-  // };
-
-  // const isMultiple = count % 5 === 0 && count !== 0 ? true : false;
-  // const countType = isMultiple ? "Es multiplo de 5" : "No es multiplo de 5";
-  // const countClass = isMultiple ? "active" : "inactivo";
-
-
-  // const handleClick = () => {
-  //   setCount(count + 1)
-
-  // } 
-
-  // const handleClick2 = () => {
-  //   setCount(count - 1)
-
-  // } 
-  // const squareClass = isOn ? "square-on" : "square-off"
-  // const squareText = isOn ? "ON" : "OFF"
-  const koders = [
+  const [koders,setKoders] = useState([ 
     {
       firstName: "Luis",
       lastName: "Vera",
       age: 24,
       gender: "m",
-      photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&usqp=CAU",
+      photoURL: "https://sgame.etsisi.upm.es/pictures/18253.jpg?1621958969/",
     },
     {
       firstName: "Nestor",
       lastName: "Ramírez",
       age: 40,
       gender: "m",
-      photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&usqp=CAU",
+      photoURL: "https://sgame.etsisi.upm.es/pictures/18253.jpg?1621958969/",
     },
     {
       firstName: "David",
       lastName: "Romero",
       age: 28,
       gender: "m",
-      photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&usqp=CAU",
+      photoURL: "https://sgame.etsisi.upm.es/pictures/18253.jpg?1621958969/",
     },
     {
       firstName: "Yusef",
       lastName: "Lopéz",
       age: 40,
-      gender: "m",
-      photoURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&usqp=CAU",
+      gender: "f",
+      photoURL: "https://media4.giphy.com/media/5sYyfIMRcpJWNqdySh/giphy.gif",
     },
-  ];
-  // const animals = ["parrot", "dog", "breaver", "cat"];
-  // const animalsUI = animals.map((animal, index) => (
-  //   <li key={index}>{animal}</li>
-  // ));
-  // console.log(animalsUI);
+  ]);
 
-  const kodersUi = koders.map(
-    ({ firstName, lastName, age, gender, photoURL }) => (
-      <div className="card-container">
-        <div className="card-image-container">
-          <img alt="profile" src={photoURL} className="card-image" />
-        </div>
-        <div className="card-content">
-          <div className="card-item-content">
-            <p className="title">Nombre</p>
-            <p>{firstName} {lastName}</p>
-          </div>
-          <div className="card-item-content">
-            <p className="title">Edad</p>
-            <p>{age}</p>
-          </div>
-          <div className="card-item-content">
-            <p className="title">Gender</p>
-            <p>{gender}</p>
-          </div>
+  const kodersUI = koders.map(
+    ({ firstName, lastName, age, gender, photoURL }, index) => (
+      <Card
+        key={index}
+        photoURL={photoURL}
+        firstName={firstName}
+        lastName={lastName}
+        age={age}
+        gender={gender}
+      />
+    )
+  );
 
-        </div>
-      </div>
-    ))
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("AGREGAR KODER");
+    const newKoders = [
+      ...koders,
+      {
+        firstName,
+        lastName,
+        age,
+        gender,
+        photoURL,
+      },
+    ];
 
+    setKoders(newKoders);
 
+    setFirstName("");
+    setLastName("");
+    setAge("");
+    setGender("");
+    setPhotoURL("");
+  };
+
+  console.log(koders, "KODERS");
 
   return (
     <div className="App">
-      <header className="App-header">
-
-        {/* <ul>
-          <li onClick={() => setItemActive(1)} className={`${isActive(1) ? 'active' : "inactive"}`}>Getting Started</li>
-          <li onClick={() => setItemActive(2)} className={`${isActive(2) ? 'active' : "inactive"}`}>Add React to a Website</li>
-          <li onClick={() => setItemActive(3)} className={`${isActive(3) ? 'active' : "inactive"}`}>Create a New React App</li>
-          <li onClick={() => setItemActive(4)} className={`${isActive(4) ? 'active' : "inactive"}`}>CDN Links</li>
-          <li onClick={() => setItemActive(5)} className={`${isActive(5) ? 'active' : "inactive"}`}>Release Channels</li>
-
-        </ul> */}
-
-        {/* <ul>{animalsUI}</ul> */}
-
-        {kodersUi}
-
-        {/* <Title text="Aaron"></Title>
-
-        <FullName firstName="Yusef" lastName="Lopez"></FullName>
-
-        <p className={countClass}>
-          Counter: {count}, {countType}
-        </p>
-
-        <button onClick={() => handleClick(true)}>Increase</button> */}
-
-        {/* <button onClick={handleClick}>Increase</button>
-        <button onClick={handleClick2}>Decrease</button> */}
-        {/* TEMPLATE INTERPOLACION */}
-        {/* <div className={`square ${isOn ? "square-on" : "square-off"}`}>{isOn ? "ON" : "OFF"}
-        </div>   */}
-        {/* <div className={`square ${squareClass}`}
-             onMouseOver={() => setIsOn(!isOn)}
-        >
-          {squareText}
-        </div>
-
-       
-        <div
-          style={{
-            display: "flex",
-          }}
-        >
-          {/* <button onClick={() => setIsOn(!isOn)}>Interruptor</button>
-                   */}
-        {/* <button onClick={() => setIsOn(true)}>ON</button>
-          <button onClick={() => setIsOn(false)}>OFF</button> */}
-
-        {/* </div> */}
-
-      </header>
+      <div className="main-container">
+        <div className="container">{kodersUI}</div>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+        <input
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+        <input value={age} onChange={(event) => setAge(event.target.value)} />
+        <input
+          value={gender}
+          onChange={(event) => setGender(event.target.value)}
+        />
+        <input
+          value={photoURL}
+          onChange={(event) => setPhotoURL(event.target.value)}
+        />
+        <button type="submit">Agregar Koder</button>
+      </form>
     </div>
   );
 }
--- src/components/fullName/index.jsx
+++
@@ -1,7 +0,0 @@
-import './full.css';
-
-export default function FullName({firstName, lastName}) {
-    return(
-        <p>{firstName} {lastName}</p>
-    )
-}
